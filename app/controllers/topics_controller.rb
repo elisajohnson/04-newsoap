dac365276dc9e4b35dda3cce319f51536b7eed46
elisajohnson <elisaj33@yahoo.com>
2015-03-26 18:18:47 -0700

    HTTPARTY get printing to view as hash

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,15 +1,13 @@
 class TopicsController < ApplicationController
   def index
-    @user_topics = [
-    @base_url = 'http://api.npr.org/query?',
-    #'id=1137' //grace hopper
-    @query = 'id=3002',
-    @access_token = '&apiKey=' + ENV['NPR_API_KEY'],
-    @response = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story'][i]['title'],
-    @teaser_response = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story'][i]['teaser'],
-    @link_response = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story'][i]['link'][0]['__content__']
-    ]
-    # @response = HTTParty.get("http://api.npr.org/query?id=1137&apiKey=" + ENV['NPR_API_KEY'])
+    @base_url = 'http://api.npr.org/query?'
+    @query = 'id=3002'
+    @access_token = '&apiKey='+ENV['NPR_API_KEY']
+    @response = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story']
+
+
+
+    # @response = HTTParty.get("http://api.npr.org/query?id=1149,1059&apiKey=" + ENV['NPR_API_KEY'])['nprml']['list']['story']
     # render json: @response
   end
 
