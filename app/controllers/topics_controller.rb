c02af9a7a412c91f02d2faf127e0903f02029a79
elisajohnson <elisaj33@yahoo.com>
2015-03-30 14:46:22 -0700

    id's in the topics index are now properly assigned

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,30 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    if user_signed_in?
+      redirect_to welcome_index_path
+    else
+      @topics = Topic.all
+    end
+
+    # @topic = Topic.new
+
+    # @base_url = 'http://api.npr.org/query?id='
+    # @query = :query.to_i
+    # @access_token = '&apiKey='+ENV['NPR_API_KEY']
+    # @responsie = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story']
+
     # @response = HTTParty.get("http://api.npr.org/query?id=1137&apiKey=" + ENV['NPR_API_KEY'])['nprml']['list']
     # render json: @response
+    #   end
   end
 
   def create
+    @topic = Topic.new
+    if @topic.save
+      redirect_to topics_dashboard_path
+    else
+      redirect_to welcome_index_path
+    end
   end
 
   def update
@@ -19,5 +38,9 @@ class TopicsController < ApplicationController
     @query = 'id=3004'
     @access_token = '&apiKey='+ENV['NPR_API_KEY']
     @responsie = HTTParty.get(@base_url+@query+@access_token)['nprml']['list']['story']
+  end
+
+  def topic_params
+    params.require(:topic).permit(:npr_id)
   end
 end
